110a248bccbc59701936b2d734d5045848f8a0db
Donvictory <donvictoryadewumi4@gmail.com>
2026-05-16 13:33:34 +0100

    fix: changed path name

---
+++ src/app/(public)/privacy-policy/page.tsx
@@ -0,0 +1,10 @@
+import { PrivacyPolicy } from "@/components/privacy_policy/privacy-policy";
+
+export const metadata = {
+  title: "Privacy Policy",
+  description: "Privacy Policy - Anvila",
+};
+
+export default function PrivacyPolicyPage() {
+  return <PrivacyPolicy />;
+}
