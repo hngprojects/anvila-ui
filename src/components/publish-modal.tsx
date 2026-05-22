b69adfaf29d668df0377a31bfccd0d63ca282f2b
Donvictory <donvictoryadewumi4@gmail.com>
2026-05-22 21:56:35 +0100

    fix: fixed lint error in publish-modal file

--- src/components/publish-modal.tsx
+++ src/components/publish-modal.tsx
@@ -14,7 +14,7 @@ interface GithubPublishModalProps {
 
 const DownloadSVG = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-    <path d="M19 13V17C19 17.5304 18.7893 18.0391 18.4142 18.4142C18.0391 18.7893 17.5304 19 17 19H3C2.46957 19 1.96086 18.7893 1.58579 18.4142C1.21071 18.0391 1 17.5304 1 17V13M15 8L10 13L5 8M10 13V1" stroke="#E6EFEF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M19 13V17C19 17.5304 18.7893 18.0391 18.4142 18.4142C18.0391 18.7893 17.5304 19f 17 19H3C2.46957 19 1.96086 18.7893 1.58579 18.4142C1.21071 18.0391 1 17.5304 1 17V13M15 8L10 13L5 8M10 13V1" stroke="#E6EFEF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
 
