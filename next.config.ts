094e58106496a3f85960b508fcd78ee90f19b25a
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-06-02 19:52:27 +0100

    fix: add proxy rewrite to fix CORS on staging

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
