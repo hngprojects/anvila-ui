64a401bcefc716633b27419ce298edef633f761d
Donvictory <donvictoryadewumi4@gmail.com>
2026-06-06 10:06:27 +0100

    fix: removed copy-icon

--- src/components/protected/generator/chat-view-item.tsx
+++ src/components/protected/generator/chat-view-item.tsx
@@ -43,9 +43,7 @@ export function ChatItemView({
             }
             className="shrink-0"
             aria-label="Copy message"
-          >
-            <ChatCopyIcon />
-          </button>
+          ></button>
         </div>
       </div>
     );
