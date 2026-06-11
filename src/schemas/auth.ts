a2b9b34b1187d32c934926e8698a0602c85b22f1
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-06-11 12:50:41 +0100

    fix: display name input box

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -11,7 +11,11 @@ export const LoginSchema = z.object({
 
 export const RegisterSchema = z
   .object({
-    display_name: z.string().min(2, "Full name must be at least 2 characters"),
+    display_name: z
+      .string()
+      .min(5, "Display name must be at least 5 characters")
+      .regex(/[a-zA-Z]/, "Must contain at least one letter")
+      .regex(/^[\p{L}\p{N}\s_-]+$/u, "Only letters, numbers, spaces, hyphens and underscores allowed"),
     email: z
       .string()
       .regex(emailRegex, { message: "Please enter a valid email" }),
