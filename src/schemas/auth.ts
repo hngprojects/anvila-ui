d51f899b420154abfedc2567fc1dbe0cfaaab271
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-06-11 13:13:54 +0100

    fix: allow unicode letters

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -14,9 +14,12 @@ export const RegisterSchema = z
     display_name: z
       .string()
       .min(3, "Display name must be at least 3 characters")
-      .regex(/^\D/, "Cannot start with a number")   
-      .regex(/[a-zA-Z]/, "Must contain at least one letter")
-      .regex(/^[\p{L}\p{N}\s_-]+$/u, "Only letters, numbers, spaces, hyphens and underscores allowed"),
+      .regex(/^\D/, "Cannot start with a number")
+      .regex(/\p{L}/u, "Must contain at least one letter")
+      .regex(
+        /^[\p{L}\p{N} _-]+$/u,
+        "Only letters, numbers, spaces, hyphens and underscores allowed",
+      ),
     email: z
       .string()
       .regex(emailRegex, { message: "Please enter a valid email" }),
